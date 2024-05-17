5bb4070a8fbcd39c841e4f1882727d4d0e973d85
Spoonail-Iroiro <38171201+Spoonail-Iroiro@users.noreply.github.com>
2024-05-18 04:12:09 +0900

    Changed to src/ layout

--- src/gmaginai_l/dirs.py
+++ src/gmaginai_l/dirs.py
@@ -5,4 +5,4 @@ from pathlib import Path
 if getattr(sys, "frozen", False):
     application_dir = Path(sys._MEIPASS).parent  # type: ignore[attr-defined]
 else:
-    application_dir = Path(__file__).parent.parent
+    application_dir = Path(__file__).parent.parent.parent
