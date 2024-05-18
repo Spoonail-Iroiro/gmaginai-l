9e3598ae97ae26f2f3edc624b4652b4667d1ba8a
Spoonail-Iroiro <38171201+Spoonail-Iroiro@users.noreply.github.com>
2024-05-18 14:08:10 +0900

    Add mods_load.js service

---
+++ tests/standard/core/test_mods_load_js_service.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from typing import Optional, List, Tuple, Dict
+import logging
+import pytest
+from gmaginai_l.core.mods_load_js_service import ModsLoadJsService
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def service():
+    return ModsLoadJsService()
+
+
+def test_to_dict(service):
+    script = """
+    // my mods
+    LOADDATA = {
+      mods: [
+        "sample",
+        "atrack",
+        // "saves"
+      ]
+    }
+    """
+
+    assert service.to_dict(script) == {"mods": ["sample", "atrack"]}
+
+    script = """
+    // my mods
+    var LOADDATA = {
+      mods: [
+        "sample",
+        "atrack",
+        // "saves"
+      ]
+    }
+
+    """
+    assert service.to_dict(script) == {"mods": ["sample", "atrack"]}
